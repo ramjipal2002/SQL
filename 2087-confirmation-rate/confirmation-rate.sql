833fa0ce7cbfac625f460e5199d3524b8df99cf5
Ram ji Pal <ramjipal36@gmail.com>
2026-05-22 18:31:12 +0530

    Time: 638 ms (11.16%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 select  s.user_id,
-round (count(Case when c.action = 'confirmed' then 1 else null end )*1.0 /count(s.user_Id),2) as confirmation_rate
+round (sum(Case when c.action = 'confirmed' then 1 else 0 end )*1.0 /count(1),2) as confirmation_rate
 from signups as s 
 left join confirmations as c on s.user_Id = c.user_Id
 group by s.user_ID
